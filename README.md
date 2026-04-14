9ffbbec5b7ab2eedc9fc22cb5d1760096a4c4369
jillvillany <jillvillany@gmail.com>
2026-04-14 10:22:47 -0400

    using openai vs ollama

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 ## Quickstart
 - clone repo
-- Download Ollama
+- Create an OpenAI API Key
+    - Log into OpenAI API Platform
+    - Click "Create API Keys"
+- Create a `.env` file that looks like the below
+```
+OPENAI_API_KEY={insert key here}
+```
 - `ollama run llama3`
 - `uv venv --python=3.12`
 - `uv sync`
--- ai_researcher/agents/research_agent.py
+++ ai_researcher/agents/research_agent.py
@@ -1,45 +1,38 @@
-from langchain_community.chat_models import ChatOllama
-from langchain_classic.agents import create_structured_chat_agent
-from langchain_classic.agents.agent import AgentExecutor
-from langchain_classic import hub
-
+from langchain_openai import ChatOpenAI
 from ai_researcher.tools.research_tools import (
     search_ai_research,
     summarize_with_citations,
 )
 
 
-def run_research_agent():
+# register tools
+tools = [
+    search_ai_research,
+    summarize_with_citations
+]
 
-    llm = ChatOllama(
-        model="llama3",
-        temperature=0
-    )
+tool_map = {
+    tool.name: tool
+    for tool in tools
+}
+
+
+
+def run_research_agent():
 
     tools = [
         search_ai_research,
         summarize_with_citations
     ]
 
-    prompt = hub.pull("hwchase17/structured-chat-agent")
+    llm = ChatOpenAI(
+    model="gpt-4o",
+        temperature=0
+    ).bind_tools(tools)
 
-    agent = create_structured_chat_agent(
-        llm,
-        tools,
-        prompt
-    )
+    response = llm.invoke("Find the latest AI research and summarize it with citations.")
 
-    executor = AgentExecutor(
-        agent=agent,
-        tools=tools,
-        verbose=True
-    )
-
-    response = executor.invoke({
-        "input": "Find the latest AI research and summarize it with citations."
-    })
-
-    return response["output"]
+    return response
 
 
 if __name__ == "__main__":
